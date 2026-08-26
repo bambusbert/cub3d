ca2c82bccdce8424559129c7334124694f073651
fsitter42 <fsitter@student.42vienna.com>
2026-08-26 12:20:35 +0200

    norm db hash

--- f_db/f_db_hash.c
+++ f_db/f_db_hash.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 15:17:22 by fsitter           #+#    #+#             */
-/*   Updated: 2026/07/29 15:31:19 by fsitter          ###   ########.fr       */
+/*   Updated: 2026/08/26 12:20:02 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,16 @@ uint32_t	f_db_hash0(const char *key)
 	return (hash);
 }
 
-uint32_t	f_db_hash1(const char *key)
-{
-	uint32_t	hash;
+// uint32_t	f_db_hash1(const char *key)
+// {
+// 	uint32_t	hash;
 
-	hash = 2166136261U;
-	while (*key)
-	{
-		hash ^= (uint8_t) * key;
-		hash *= 16777619U;
-		key++;
-	}
-	return (hash);
-}
-
-// TODO eine loeschen
+// 	hash = 2166136261U;
+// 	while (*key)
+// 	{
+// 		hash ^= (uint8_t) * key;
+// 		hash *= 16777619U;
+// 		key++;
+// 	}
+// 	return (hash);
+// }
