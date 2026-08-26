7e276b7ff6e8cacceb9535e7d9136556af1d81a4
fsitter42 <fsitter@student.42vienna.com>
2026-08-26 12:13:24 +0200

    norm parse cols1 and 2

--- src/f_parse_colors.c
+++ src/f_parse_colors.c
@@ -3,100 +3,99 @@
 /*                                                        :::      ::::::::   */
 /*   f_parse_colors.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slambert <slambert@student.42vienna.com    +#+  +:+       +#+        */
+/*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 15:12:51 by fsitter           #+#    #+#             */
-/*   Updated: 2026/08/20 17:57:24 by slambert         ###   ########.fr       */
+/*   Updated: 2026/08/26 12:12:33 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/f_parsing.h"
 
-bool f_extract_colors(t_db *db, t_color *colors);
-bool f_border_control(t_color *colors);
-char **f_free_rgb(char **rgb, int error_code);
+bool	f_extract_colors(t_db *db, t_color *colors);
+bool	f_border_control(t_color *colors);
+char	**f_free_rgb(char **rgb, int error_code);
 
-bool f_init_colors(t_color *colors)
+bool	f_init_colors(t_color *colors)
 {
-    colors[0].key = 'F';
-    colors[1].key = 'C';
-    return (true);
+	colors[0].key = 'F';
+	colors[1].key = 'C';
+	return (true);
 }
 
-bool f_parse_colors(t_db *db, t_color *colors)
+bool	f_parse_colors(t_db *db, t_color *colors)
 {
-    f_init_colors(colors);
-    if (f_comma_after_comma(db) == false)
-    {
-        ft_putstr_fd("Error\nWrong color format\n", 2);
-        return (false);
-    }
-    if (f_extract_colors(db, colors) == false)
-        return (false);
-    return (f_border_control(colors));
+	f_init_colors(colors);
+	if (f_comma_after_comma(db) == false)
+	{
+		ft_putstr_fd("Error\nWrong color format\n", 2);
+		return (false);
+	}
+	if (f_extract_colors(db, colors) == false)
+		return (false);
+	return (f_border_control(colors));
 }
 
-bool f_border_control(t_color *colors)
+bool	f_border_control(t_color *colors)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < 2)
-    {
-        if (colors[i].r == 600)
-            return (false);
-        if (colors[i].g == 600)
-            return (false);
-        if (colors[i].b == 600)
-            return (false);
-        i++;
-    }
-    return (true);
+	i = 0;
+	while (i < 2)
+	{
+		if (colors[i].r == 600)
+			return (false);
+		if (colors[i].g == 600)
+			return (false);
+		if (colors[i].b == 600)
+			return (false);
+		i++;
+	}
+	return (true);
 }
 
-
-bool f_extract_colors(t_db *db, t_color *colors)
+bool	f_extract_colors(t_db *db, t_color *colors)
 {
-    char key_str[2];
-    const char *val;
-    char **rgb;
-    int i;
-    
-    key_str[1] = '\0';
-    i = -1;
-    while (++i < 2)
-    {
-        key_str[0] = colors[i].key;
-        val = db_get(db, key_str);
-        if (!val)
-            return (false);
-        rgb = ft_split(val, ',');
-        if (!rgb)
-            return (false);
-        if (!rgb[0] || !rgb[1] || !rgb[2] || rgb[3] != NULL)
-            return (f_free_rgb(rgb, 1), false);
-        colors[i].r = f_catch_num(rgb[0]);
-        colors[i].g = f_catch_num(rgb[1]);
-        colors[i].b = f_catch_num(rgb[2]);
-        rgb = f_free_rgb(rgb, 0);
-    }
-    return (true);
+	char		key_str[2];
+	const char	*val;
+	char		**rgb;
+	int			i;
+
+	key_str[1] = '\0';
+	i = -1;
+	while (++i < 2)
+	{
+		key_str[0] = colors[i].key;
+		val = db_get(db, key_str);
+		if (!val)
+			return (false);
+		rgb = ft_split(val, ',');
+		if (!rgb)
+			return (false);
+		if (!rgb[0] || !rgb[1] || !rgb[2] || rgb[3] != NULL)
+			return (f_free_rgb(rgb, 1), false);
+		colors[i].r = f_catch_num(rgb[0]);
+		colors[i].g = f_catch_num(rgb[1]);
+		colors[i].b = f_catch_num(rgb[2]);
+		rgb = f_free_rgb(rgb, 0);
+	}
+	return (true);
 }
 
-char **f_free_rgb(char **rgb, int error_code)
+char	**f_free_rgb(char **rgb, int error_code)
 {
-    int i;
-    
-    i = 0;
-    if (!rgb)
-        return (NULL);
-    while (rgb[i])
-    {
-        free (rgb[i]);
-        i++;
-    }
-    free (rgb);
-    if (error_code != 0)
-        ft_putstr_fd("Error\nWrong color format\n", 2);
-    return (NULL);
+	int	i;
+
+	i = 0;
+	if (!rgb)
+		return (NULL);
+	while (rgb[i])
+	{
+		free(rgb[i]);
+		i++;
+	}
+	free(rgb);
+	if (error_code != 0)
+		ft_putstr_fd("Error\nWrong color format\n", 2);
+	return (NULL);
 }
